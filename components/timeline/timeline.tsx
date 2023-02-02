4d99b4c4926bed03fb5ef5e823c3a792eef9067f
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-02 00:05:22 -0500

    adding styling to timeline

--- components/timeline/timeline.tsx
+++ components/timeline/timeline.tsx
@@ -1,67 +1,67 @@
 const Timeline = () => {
     return (
-        <div className="relative my-0 mx-auto w-full">
-            <div className="relative py-2.5 px-10 w-2/4 right-1/2">
+        <div className="relative my-0 mx-auto w-full after:content-[''] after:absolute after:w-1.5 after:bg-cyan-200 after:top-0 after:bottom-0 after:left-1/2 after:-ml-1">
+            <div className="relative py-2.5 px-10 w-2/4 left-1/2 after:content-[' '] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10 after:-left-4">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2021</h2>
-                <h4>Lead Front End Engineer</h4>
-                <p>{`Drive front end development in React and Typescript of Cengage's secondary platform Mind Tap Schools for K-12. Build compound component library shared by multiple application along with maintaing company component library React Magma.  Lead front end technology decisions for best practices.`}</p>
+                <h2 className="text-slate-500">2021</h2>
+                <h4 className="text-slate-500">Lead Front End Engineer</h4>
+                <p className="text-slate-500">{`Drive front end development in React and Typescript of Cengage's secondary platform Mind Tap Schools for K-12. Build compound component library shared by multiple application along with maintaing company component library React Magma.  Lead front end technology decisions for best practices.`}</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 left-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 -left-2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2021</h2>
-                <h4>Front End Developer Project</h4>
-                <p>National Greographic Learning K-12 Big Ideas Mathematics campaign.</p>
+                <h2 className="text-slate-500">2021</h2>
+                <h4 className="text-slate-500">Front End Developer Project</h4>
+                <p className="text-slate-500">National Greographic Learning K-12 Big Ideas Mathematics campaign.</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 right-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 left-1/2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10 after:-left-4">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2021</h2>
-                <h4>Front End Developer Project</h4>
-                <p>National Greographic Learning K-12 digital Access and Integration campaign.</p>
+                <h2 className="text-slate-500">2021</h2>
+                <h4 className="text-slate-500">Front End Developer Project</h4>
+                <p className="text-slate-500">National Greographic Learning K-12 digital Access and Integration campaign.</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 left-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 -left-2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2021</h2>
-                <h4>Front End Developer Project</h4>
-                <p>National Geographic Learning Marketing site for the K-12 and secondary market.</p>
+                <h2 className="text-slate-500">2021</h2>
+                <h4 className="text-slate-500">Front End Developer Project</h4>
+                <p className="text-slate-500">National Geographic Learning Marketing site for the K-12 and secondary market.</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 right-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 left-1/2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10 after:-left-4">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2020</h2>
-                <h4>Front End Developer Project</h4>
-                <p>Maintenance and development of Creative Studio Wordpress Site.</p>
+                <h2 className="text-slate-500">2020</h2>
+                <h4 className="text-slate-500">Front End Developer Project</h4>
+                <p className="text-slate-500">Maintenance and development of Creative Studio Wordpress Site.</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 left-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 -left-2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2017 - 2021</h2>
-                <h4>Senior Software Engineer</h4>
-                <p>Full stack web development in Java, JavaScript with Struts, Angular, and React frameworks on MyELT platform. Maintenance and enhancements of Speech Recognition Software. Automation of JSpeech Grammar Formatting creation and evaluation in React. Authoring Content Companion tool development in React.</p>
+                <h2 className="text-slate-500">2017 - 2021</h2>
+                <h4 className="text-slate-500">Senior Software Engineer</h4>
+                <p className="text-slate-500">Full stack web development in Java, JavaScript with Struts, Angular, and React frameworks on MyELT platform. Maintenance and enhancements of Speech Recognition Software. Automation of JSpeech Grammar Formatting creation and evaluation in React. Authoring Content Companion tool development in React.</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 right-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 left-1/2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10 after:-left-4">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2016 - 2017</h2>
-                <h4>Software Engineer</h4>
-                <p>Development, Maintenance and Enhancement of online learning software platform <a href="https://myelt.heinle.com/ilrn/authentication/signIn.do?inst=MYELT">myELT</a>.</p>
+                <h2 className="text-slate-500">2016 - 2017</h2>
+                <h4 className="text-slate-500">Software Engineer</h4>
+                <p className="text-slate-500">Development, Maintenance and Enhancement of online learning software platform <a href="https://myelt.heinle.com/ilrn/authentication/signIn.do?inst=MYELT">myELT</a>.</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 left-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 -left-2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2015 - 2017</h2>
-                <h4>Technology Analyst</h4>
-                <p>Development and Maintenance of Float Analysis Web application tool using HTML5, CSS3, and JavaScript. Development and Maintenance on Operations Support Web Troubleshooting Guide across 30 applications.  Maintenance of HTML5, CSS3 and JavaScript across these web tools incorporating Bootstrap Framework.</p>
+                <h2 className="text-slate-500">2015 - 2017</h2>
+                <h4 className="text-slate-500">Technology Analyst</h4>
+                <p className="text-slate-500">Development and Maintenance of Float Analysis Web application tool using HTML5, CSS3, and JavaScript. Development and Maintenance on Operations Support Web Troubleshooting Guide across 30 applications.  Maintenance of HTML5, CSS3 and JavaScript across these web tools incorporating Bootstrap Framework.</p>
                 </div>
             </div>
-            <div className="relative py-2.5 px-10 w-2/4 right-1/2">
+            <div className="relative py-2.5 px-10 w-2/4 left-1/2 after:content-[''] after:absolute after:w-6 after:-right-4 after:bg-black after:rounded-full after:border-4 after:border-cyan-200 after:top-4 after:z-10 after:-left-4">
                 <div className="relative py-5 px-8 rounded-md bg-cyan-200">
-                <h2>2004 - 2015</h2>
-                <h4>Business Analyst</h4> 
-                <p>Developed operational solutions to support the End to End Systems integration for the Payment &amp; Deposit Operations departments. Developed End to End operational process flows and Business requirements converting paper processes to virtual proceses improving distribution of funds from 3 day average to 12 hours.</p>
+                <h2 className="text-slate-500">2004 - 2015</h2>
+                <h4 className="text-slate-500">Business Analyst</h4> 
+                <p className="text-slate-500">Developed operational solutions to support the End to End Systems integration for the Payment &amp; Deposit Operations departments. Developed End to End operational process flows and Business requirements converting paper processes to virtual proceses improving distribution of funds from 3 day average to 12 hours.</p>
                 </div>
             </div>
         </div>
