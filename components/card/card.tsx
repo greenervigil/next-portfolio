78d6d2bc93b58753a7ad286906ada4626d490e34
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-04 00:01:27 -0500

    adding height to card

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -8,7 +8,7 @@ const Card: React.FC<CardProps> = ({
     children
 }) => {
     return (
-        <div className="w-1/2 text-black h-min text-center my-0 mx-auto pt-1 overflow-y-hidden rounded-md bg-slate-500">
+        <div className="w-1/2 text-black h-52 text-center my-0 mx-auto pt-1 overflow-y-hidden rounded-md bg-slate-500">
             {children}
         </div>
     )
