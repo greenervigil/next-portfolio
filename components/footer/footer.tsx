276ac512d19f219bc42abf6627fd125e4e4f6a2c
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-05 00:34:54 -0500

    fixing the footer to the bottom of the page

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,8 +3,8 @@ import Container from "../container/container"
 const Footer: React.FC = () => {
     return (
         <Container>
-            <footer className="flex justify-center align-baseline border-t">
-            <p className="font-mono m-3">Greener-Vigil &copy; {new Date().getFullYear()}</p>
+            <footer className="bottom-0 fixed w-full flex justify-center align-baseline border-t border-t-cyan-200">
+            <p className="font-mono m-3 ">Greener-Vigil &copy; {new Date().getFullYear()}</p>
             </footer>
         </Container>
     )
