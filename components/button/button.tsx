00919c084804226cc41cb94f3cbfaf4b2ef012f7
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-05 12:31:42 -0500

    changing color theme

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
         <button 
             type='button' 
             aria-label={ariaLabel || label} 
-            className='bg-cyan-300 rounded-lg border-cyan-400 text-black text-center text-lg h-10 w-28 shadow-md hover:bg-cyan-800 hover:text-cyan-400 hover:rounded-lg hover:shadow-2xl hover:shadow-cyan-400 font-mono' 
+            className='bg-teal-600 rounded-lg border-slate-800 text-black text-center text-lg h-10 w-auto px-4 shadow-2xl shadow-slate-700 hover:bg-teal-900 hover:text-white hover:rounded-lg hover:shadow-2xl hover:shadow-teal-600 font-mono' 
             onClick={handleOnclick}
         >
             {label}
