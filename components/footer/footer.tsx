f37b45090f204a40de52a363a011c7690c1c04cf
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-05 00:18:56 -0500

    updating footer font

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
     return (
         <Container>
             <footer className="flex justify-center align-baseline border-t">
-            <p className="m-3">Greener-Vigil &copy; {new Date().getFullYear()}</p>
+            <p className="font-mono m-3">Greener-Vigil &copy; {new Date().getFullYear()}</p>
             </footer>
         </Container>
     )
