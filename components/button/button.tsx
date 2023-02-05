efbe892460ef8f605e9f75c61f8d5583782a5b82
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-05 00:18:34 -0500

    changes to button font

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
         <button 
             type='button' 
             aria-label={ariaLabel || label} 
-            className='bg-cyan-300 rounded-lg border-cyan-400 text-black text-center text-lg h-10 w-28 shadow-md font-sans hover:bg-cyan-800 hover:text-cyan-400 hover:rounded-lg hover:shadow-2xl hover:shadow-cyan-400 font-mono' 
+            className='bg-cyan-300 rounded-lg border-cyan-400 text-black text-center text-lg h-10 w-28 shadow-md hover:bg-cyan-800 hover:text-cyan-400 hover:rounded-lg hover:shadow-2xl hover:shadow-cyan-400 font-mono' 
             onClick={handleOnclick}
         >
             {label}
