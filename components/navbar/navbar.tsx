f786c1bb4924e08a3ef66772e1c980ebc63a50ef
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-15 21:25:12 -0500

    closes #16 mobile navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -13,39 +13,60 @@ const MENU_LIST = [
 ]
 
 const Navbar: React.FC = () => {
+    const [open, setOpen] = React.useState(false)
     const [navActive, setNavActive] = React.useState(false);
     const [activeIdx, setActiveIdx] = React.useState(-1);
 
     return (
         <header className="w-full bg-white h-20 sticky z-30 top-0 shadow-lg shadow-teal-600 border border-solid border-teal-600 rounded-b-xl dark:bg-black">
-            <nav className="flex justify-between items-center text-center p-4 ml-8">
-                <Link href="/">
-                    <Image src="/../public/favicon.ico" height={50} width={50} alt="greenervigil logo"/>
-                </Link>
-                <div className="block lg:hidden">
-                    <Button
-                        aria-controls="navbar-menu"
-                        aria-label="Toggle menu"
-                        aria-expanded="false"
-                        onClick={() => setNavActive(!navActive)}
-                    >
-                        <span className="block w-6 h-1 m-0.5 bg-slate-900"></span>
-                        <span className="block w-6 h-1 m-0.5 bg-slate-900"></span>
-                        <span className="block w-6 h-1 m-0.5 bg-slate-900"></span>
-                    </Button>
+            <nav className="font-mono flex filter drop-shadow-md bg-white dark:bg-black px-4 py-4 h-20 items-center">
+                <div className={`absolute top-0 left-0 h-screen w-screen bg-white dark:bg-black transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
+                    <div className="flex items-center justify-center filter drop-shadow-md bg-white dark:bg-black h-20"> 
+                        {/*logo container*/}
+                        <Link href="/"><a className="text-xl font-semibold">DJGV</a></Link>
+                    </div>
+                    <div className="flex flex-col text-center mt-7 gap-5">
+                        {MENU_LIST.map((menu, idx) => (
+                            <div
+                                onClick={() => {
+                                    setActiveIdx(idx);
+                                    setNavActive(false);
+                                    setOpen(false)
+                                }}
+                                key={menu.text}
+                            >
+                                <NavItem active={activeIdx === idx} {...menu} />
+                            </div>
+                        ))}
+                    </div>  
                 </div>
-                <div className={`${navActive ? "active" : ""} flex gap-7 right-10 font-mono`}>
-                    {MENU_LIST.map((menu, idx) => (
-                        <div
-                            onClick={() => {
-                                setActiveIdx(idx);
-                                setNavActive(false);
-                            }}
-                            key={menu.text}
-                        >
-                            <NavItem active={activeIdx === idx} {...menu} />
-                        </div>
-                    ))}
+                <div className="w-3/12 flex items-center">
+                    <Link href="/"><a className="text-3xl font-semibold">DJGV</a></Link>
+                </div>
+                <div className="w-9/12 flex justify-end items-center">
+
+                    <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden" onClick={() => {
+                        setOpen(!open)
+                    }}>
+                        {/* hamburger button */}
+                        <span className={`h-1 w-full bg-black dark:bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
+                        <span className={`h-1 w-full bg-black dark:bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
+                        <span className={`h-1 w-full bg-black dark:bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
+                    </div>
+
+                    <div className="hidden md:flex md:gap-10">
+                        {MENU_LIST.map((menu, idx) => (
+                            <div
+                                onClick={() => {
+                                    setActiveIdx(idx);
+                                    setNavActive(false);
+                                }}
+                                key={menu.text}
+                            >
+                                <NavItem active={activeIdx === idx} {...menu} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </nav>
         </header>
