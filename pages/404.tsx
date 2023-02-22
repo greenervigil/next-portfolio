f6673d3617269ac11b789b0c285a4bf5ba18eb60
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-21 20:35:14 -0500

    adding 404 to try to redirect slug error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from "next";
 
-const Cusom404: NextPage = () => {
+const Custom404: NextPage = () => {
   window.location.href = 'next-portfolio/#' + window.location.pathname.replace('/next-portfolio', '')
-  
+
   return <></>
 }
+
+export default Custom404
