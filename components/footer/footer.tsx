b4ce9489cc19e8facb90eab00f8fa800bbda39c8
greenervigil <daniel.julian.vigil@gmail.com>
2023-02-22 02:04:38 +0000

    Deploying to public from @ greenervigil/next-portfolio@387904352098d6040dccda626d3a75e0461e0222 🚀

--- components/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer: React.FC = () => {
-    return (
-        <footer className="bottom-0 w-full flex justify-center align-baseline border-t border-t-teal-600 text-slate-500">
-            <p className="font-mono m-3 ">Greener-Vigil &copy; {new Date().getFullYear()}</p>
-        </footer>
-    )
-}
-
-export default Footer
--- types/user-type.ts
+++
@@ -1,9 +0,0 @@
-import { ProjectType } from "./project-type";
-
-export type UserType = {
-  name: string;
-  title: string;
-  aboutText: string;
-  greenfield: string;
-  projects: ProjectType[];
-}
