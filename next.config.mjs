fdff240f2ec84ec0a1e13dc25913cd1e366ef1f2
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-21 20:01:16 -0500

    adding some updates to the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   images: {
     loader: 'akamai',
@@ -51,7 +64,8 @@ const nextConfig = {
       }
     ]
   }, 
-  assetPrefix: './'
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 export default nextConfig;
