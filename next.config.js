8dd351f47549e1948ca332e455b42cbd73f9dff5
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-22 21:48:31 -0500

    updating config for navigation in ghpages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
