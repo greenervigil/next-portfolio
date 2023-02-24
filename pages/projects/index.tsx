3eddf61c0f6a63b1a2bc68e8e67a830e03f28025
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-24 01:08:11 -0500

    starting the filter pills on the projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,19 +1,40 @@
 import { NextPage } from "next";
+import React from "react";
 import Card2 from "../../components/card/card-2";
 import Container from "../../components/container/container";
 import { useAppContext } from "../../context/state";
 
 const Projects: NextPage = () => {
-
     const data = useAppContext()
+    const [filterData, setFilterData] = React.useState(data.projects);
+    const tech = ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Java', 'React', 'NextJS', 'Bootstrap', 'Wordpress']
+
+    const handleFilter = (e: any) => {
+        e.preventDefault()
+        setFilterData(e.target.innerHTML)
+    }
 
     return (
         <>
             <Container>
                 <h1 className="font-mono text-center text-6xl mt-16">Projects</h1>
+                <section className="text-center m-5">
+                    <p className="m-5">Select an option to filter:</p>
+                    {tech.map(item => {
+                        return (
+                            <button 
+                                key={item} 
+                                className="pointer text-sm rounded-2xl m-1 py-2 px-4 border border-teal-600 hover:bg-teal-600"
+                                onClick={handleFilter}
+                            >
+                                {item}
+                            </button>
+                        )
+                    })}
+                </section>
             </Container>
             <div className="flex justify-center gap-3 flex-wrap max-w-5xl">
-                {data.projects.map((project, i) => {
+                {filterData.map((project, i) => {
                     return <Card2 data={project} key={i} />
                 })}
             </div>
