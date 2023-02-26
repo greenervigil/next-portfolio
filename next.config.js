445fa57009c87cb5ef06297b254617c87ebd6ea2
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-02-26 14:57:10 -0500

    build error due to adding lang

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
+  exportPathMap: true,
   swcMinify: true,
   i18n: {
     locales: ['en'],
