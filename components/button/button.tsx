1021c06b214b70bd9a9bda907ce5f740bf7c968b
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-03-01 21:57:46 -0500

    updating the button for a11y

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -15,7 +15,7 @@ const Button: React.FC<ButtonProps> = ({
         <button 
             type='button' 
             aria-label={ariaLabel || label} 
-            className='bg-yellow-500 text-slate-900 text-center rounded-lg shadow-2xl shadow-teal-600 min-w-36 h-9 px-4 hover:bg-yellow-600 hover:text-white hover:shadow-teal-500' 
+            className='bg-yellow-500 text-slate-900 text-center rounded-lg shadow-2xl shadow-slate-900 min-w-36 h-9 px-4 hover:bg-yellow-600 hover:text-white hover:shadow-teal-500' 
         >
             {children}
         </button>
