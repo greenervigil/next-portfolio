38876ac8c5c28de76453bdbda8cc3826a3682e7f
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-05-04 01:09:22 -0400

    adding a thank you page for the contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -23,6 +23,7 @@ const Contact: NextPage = () => {
                         <Input id="email" label="Email" type="email" name="email" required />
                         <Input id="number" label="Phone Number" type="phone" name="phone" maxLength={13} />
                         <input type="hidden" name="_autoresponse" value="Thank you for reaching out.  Please allow up to 24 hours for a response.  I will be in touch soon."></input>
+                        <input type="hidden" name="_next" value="https://yourdomain.co/next-portfolio/thank-you"></input>
                         <Textarea required rows={13} name="message"/>
                         <button className="m-4 text-center text-neutral-900 border border-yellow-500 rounded-lg shadow-2xl shadow-slate-900 w-44 h-9 p-1 hover:bg-yellow-100 hover:text-slate-900" type="submit">Send</button>
                     </form>
---
+++ pages/thank-you.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Container from "../components/container/container";
+
+const ThankYou: NextPage = () => {
+    return (
+        <div className="font-mono text-center text-6xl mt-16">
+            <h1>Thank You</h1>
+
+            <Container>
+                        <p className="font-mono text-neutral-900">Thank you for reaching out.  Please allow up to 24 hours for a response.  I will be in touch soon. </p>
+                    </Container>
+        </div>
+    )
+}
+
+export default ThankYou;
