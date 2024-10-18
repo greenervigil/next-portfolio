e27658e1315e4ffc741153f9c2a902570c4330fa
greenervigil <daniel_greenervigil@icloud.com>
2024-10-18 14:31:41 +0000

    Deploying to public from @ greenervigil/next-portfolio@2ce2088e572759326223849ae3859c674ff5a96c 🚀

---
+++ _next/static/chunks/pages/sitemap.xml-50e4413b707b53f6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[164],{2016:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/sitemap.xml",function(){return _(6783)}])},6783:function(n,u,_){"use strict";_.r(u),u.default=()=>null}},function(n){n.O(0,[888,774,179],function(){return n(n.s=2016)}),_N_E=n.O()}]);
