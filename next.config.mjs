ed8e875185bf9505bf982818ba63647266f2b6bb
Daniel Vigil <daniel_greenervigil@icloud.com>
2026-06-02 10:00:18 -0400

    update to SEO errors for www redirect

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.greenervigil.dev" }],
+        destination: "https://greenervigil.dev/:path*",
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig
